a1045a301f8d2dd4a6cf17bb57924f4560861ab8
Emiliano <emilianogaucher@gmail.com>
2022-11-21 16:14:56 -0300

    sexto commit

--- js/carrito2.js
+++ js/carrito2.js
@@ -1,49 +1,20 @@
 const contenedorProductos = document.querySelector('#contenedorProductos');
-const contenedorCarrito = document.querySelector('#contenedorCarrito');
-const carrito = document.querySelector('#carrito');
+const URL = 'js/productos.json';
 
+//CARRITO
 let carritoProducto = [];
 
-
 //FUNCIONES
-cargarClicks();
-function cargarClicks(){
-
-    //Agregar productos presionando "agregar al carrito"
-
-    contenedorProductos.addEventListener("click", agregarProducto);
-}
-
-//agrega al carrito haciendo click en el boton
-
-function agregarProducto(e){
-    e.preventDefault();
-    if(e.target.classList.contains("agregarCarrito")){
-        const productoSeleccion = e.target.parentElement.parentElement;
-        informacionProducto(productoSeleccion);
-    }
-}
-
-//Creamos funcion productoSeleccion para leer el interior del contenedor del producto
-
-function informacionProducto(producto){
-
-    const informacionProducto={
-        imagen: producto.querySelector("img").src,
-        nombre: producto.querySelector("h5").textContent,
-        precio: producto.querySelector("p").textContent,
-        id: producto.querySelector("button").getAttribute("data-id"),
-        cantidad: 1,
-    }
-    console.log(informacionProducto);
-}
 
 //Mostrar Productos en HTML
+async function mostrarHTML() {
+  const respuesta = await fetch(URL);
+  const productos = await respuesta.json();
+  console.log(productos);
 
-function mostrarProductosHTML() {
-productos.forEach((producto) => {
+  productos.forEach((producto) => {
     const card = document.createElement('div');
-    card.classList.add("col-xl-3", "col-md-6", "col-xs-12");
+    card.classList.add('col-xl-3', 'col-md-6', 'col-xs-12');
     card.innerHTML = `
         <div class="contenedorHover">
             <figure>
@@ -51,13 +22,102 @@ productos.forEach((producto) => {
             <div class="capa">
             <h5 class="card-title"> ${producto.nombre} </h5>
             <p class="card-text"> ${producto.precio} </p>
-            <button class="btn btn-secondary agregarCarrito" data-id="${producto.id}"> Agregar al carrito </button>
+            <a class="btn btn-secondary agregarCarrito" id="boton${producto.id}"> Agregar al carrito </a>
             </div>
             </figure>
         </div>
     `;
     contenedorProductos.appendChild(card);
-});
-};
-mostrarProductosHTML();
 
+    //Agrega al carrito
+    const btnAgregarProducto = document.querySelector(`#boton${producto.id}`);
+
+    btnAgregarProducto.addEventListener('click', (e) => {
+      e.preventDefault();
+      agregaAlCarrito(producto.id);
+
+      //libreria
+      if (btnAgregarProducto) {
+        Toastify({
+          text: `Agregaste ${producto.nombre}`,
+          duration: 3000,
+          newWindow: true,
+          close: true,
+          gravity: 'bottom', // `top` or `bottom`
+          position: 'right', // `left`, `center` or `right`
+          stopOnFocus: true, // Prevents dismissing of toast on hover
+          style: {
+            background: 'linear-gradient(to right, #00b09b, #96c93d)',
+          },
+          onClick: function () {}, // Callback after click
+        }).showToast();
+      }
+    });
+  });
+}
+mostrarHTML();
+
+//Agregar al Carrito
+
+const contenedorCarrito = document.querySelector('#contenedorCarrito');
+async function agregaAlCarrito(id) {
+  const respuesta = await fetch(URL);
+  const productos = await respuesta.json();
+
+  const producto = productos.find((producto) => producto.id === id);
+  const productoCarrito = carritoProducto.find(
+    (producto) => producto.id === id
+  );
+  if (productoCarrito) {
+    productoCarrito.cantidad++;
+  } else {
+    carritoProducto.push(producto);
+  }
+
+  carritoProductoHTML();
+  console.log(carritoProducto);
+}
+
+//Mostrar Carrito en HTML
+
+function carritoProductoHTML() {
+  limpiarHTML();
+  carritoProducto.forEach((producto) => {
+    const cardCarrito = document.createElement('div');
+
+    cardCarrito.classList.add('col-xl-3', 'col-md-6', 'col-xs-12');
+    cardCarrito.innerHTML = `
+        <div class="contenedorHover">
+            <figure>
+            <img src="${producto.img}" class="card-img-top imgProductos" alt="${producto.nombre}">
+            <div class="capa">
+            <h5 class="card-title"> ${producto.nombre} </h5>
+            <p class="card-text"> ${producto.precio} </p>
+            <p class="cantidad>${producto.cantidad}</p>
+
+            </div>
+            </figure>
+        </div>
+    `;
+
+    contenedorCarrito.appendChild(cardCarrito);
+  });
+  const vaciarCarrito = document.querySelector('#vaciarCarrito');
+  vaciarCarrito.addEventListener('click', (e) => {
+    e.preventDefault();
+    carritoProducto = [];
+    carritoProductoHTML();
+  });
+}
+
+function limpiarHTML() {
+  contenedorCarrito.innerHTML = '';
+}
+
+const pagar = document.querySelector('#pagar');
+pagar.addEventListener('click', (e) => {
+  e.preventDefault();
+  Swal.fire('Pagaste!', 'Productos Enviados!', 'success');
+  carritoProducto = [];
+  carritoProductoHTML();
+});
--- js/main.js
+++ js/main.js
@@ -1,34 +1,33 @@
 //evento submit vista contacto
 
 class Cliente {
-    constructor(nombre, mail, telefono){
-        this.nombre = nombre;
-        this.mail = mail;
-        this.telefono = telefono;
-    }
+  constructor(nombre, mail, telefono) {
+    this.nombre = nombre;
+    this.mail = mail;
+    this.telefono = telefono;
+  }
 }
-const arrayClientes= [];
+const arrayClientes = [];
 
-const formulario = document.getElementById("formulario");
+const formulario = document.getElementById('formulario');
 
-formulario.addEventListener("submit", (e) => {
-    //Evito el comportamiento por default de recargar pagina al enviar formulario
-    e.preventDefault();
-    const nombre = document.getElementById("nombre");
-    const mail = document.getElementById("mail");
-    const telefono = document.getElementById("telefono")
-    console.log("El nombre ingresado es: " + nombre.value);
-    console.log("El mail ingresado es: " + mail.value);
-    console.log("EL telefono ingresado es: " + telefono.value);
-    console.log("El comentario ingresado es: " + comentarios.value)
+formulario.addEventListener('submit', (e) => {
+  //Evito el comportamiento por default de recargar pagina al enviar formulario
+  e.preventDefault();
+  const nombre = document.getElementById('nombre');
+  const mail = document.getElementById('mail');
+  const telefono = document.getElementById('telefono');
+  console.log('El nombre ingresado es: ' + nombre.value);
+  console.log('El mail ingresado es: ' + mail.value);
+  console.log('EL telefono ingresado es: ' + telefono.value);
+  console.log('El comentario ingresado es: ' + comentarios.value);
 
+  //Creamos un objeto Cliente:
+  const cliente = new Cliente(nombre.value, mail.value, telefono.value);
+  arrayClientes.push(cliente);
 
-    //Creamos un objeto Cliente:
-    const cliente = new Cliente(nombre.value, mail.value, telefono.value);
-    arrayClientes.push(cliente);
+  console.log(arrayClientes);
 
-    console.log(arrayClientes);
-
-    //Reseteamos el formulario una vez enviado:
-    formulario.reset();
-})
+  //Reseteamos el formulario una vez enviado:
+  formulario.reset();
+});
